7096f4aa52043a4c53e73fb8406452379f7d3e39
Yahya Ahmed <yahyaselim2020@gmail.com>
2026-03-05 21:34:44 +0200

    Fix: export pool and session config correctly

--- backend/config/session.js
+++
@@ -1,8 +0,0 @@
-const session = require('express-session');
-
-module.exports = session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  cookie: { maxAge: 1000 * 60 * 60 }
-});
