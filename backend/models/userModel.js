bb6d85c097e6308f28a891b4d2214590cbf9d648
Yahya Ahmed <yahyaselim2020@gmail.com>
2026-04-08 18:00:27 +0200

    Seperated function into 2, one for login and for middleware

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,20 +1,29 @@
 const db = require('../config/config');
 const bcrypt = require('bcryptjs');
 
-const findUserByEmail = async (email) => {
-  try {
-    const result = await db.query('SELECT * FROM student WHERE email = $1', [email]);
-    return result.rows[0];
-  } catch (error) {
-    throw error;
-  }
+// LOGIN
+const findUserByEmailWithPassword = async (email) => {
+  const result = await db.query(
+    'SELECT student_id, email, password FROM student WHERE email = $1',
+    [email]
+  );
+  return result.rows[0];
 };
 
+// USER DATA (middleware)
+const findUserByEmail = async (email) => {
+  const result = await db.query(
+    'SELECT student_id, first_name, last_name, email, major, gpa FROM student WHERE email = $1',
+    [email]
+  );
+  return result.rows[0];
+};
 const verifyPassword = async (plainPassword, hashedPassword) => {
   return await bcrypt.compare(plainPassword, hashedPassword);
 };
 
 module.exports = {
+  findUserByEmailWithPassword,
   findUserByEmail,
   verifyPassword
 };
